a69e9a0d9221b246afabf0a2eb0f223e27c9e14f
Glenn Searby <glenn.searby@gmail.com>
2015-11-06 14:58:33 +0000

    Deal now runs without errors.

--- game/dota_addons/card-draft/scripts/vscripts/deal.lua
+++ game/dota_addons/card-draft/scripts/vscripts/deal.lua
@@ -1,16 +1,16 @@
-abilities = 4
-ultimates = 2
-heroes = 1
+abilitiesPerHand = 4
+ultimatesPerHand = 2
+heroesPerHand = 1
 
 function deal()
-   print("Entering deal")
+   print("Entering Card Draft deal phase")
    
    -- Pause to prevent standard hero picking from happening.
    PauseGame(true)
 
    -- Load all possible options.
    local allHeroes = loadHeroes()
-   local allAbilities = loadAbilities(allHeroes)
+   local allAbilities = loadAbilities()
    local normalAbilities = allAbilities["normal"]
    local ultimates = allAbilities["ultimates"]
 
@@ -22,16 +22,25 @@ function deal()
       local i = math.random(#list)
       local dealt = list[i]
 
-      list:remove(i)
-      
+      table.remove(list, i)
+
       return dealt
    end
 
    local teams = {DOTA_TEAM_GOODGUYS, DOTA_TEAM_BADGUYS}
 
+   -- print("all abilities")
+   -- for i, ability in ipairs(normalAbilities) do
+   --    print(i, ability)
+   -- end
+   -- for i, ability in ipairs(ultimates) do
+   --    print(i, ability)
+   -- end
+
    -- Deal initial hand to each player.
    for _, team in ipairs(teams) do
       local playerCount = PlayerResource:GetPlayerCountForTeam(team)
+
       if playerCount ~= 0 then
 	 for i = 1, playerCount do
 	    local playerId = PlayerResource:GetNthPlayerIDOnTeam(team, i)
@@ -40,65 +49,92 @@ function deal()
 	    handsByPlayer[playerId] = hand
 	    picksByPlayer[playerId] = {}
 
-	    for a = 1, abilities do
+	    for a = 1, abilitiesPerHand do
 	       hand["a" .. i] = dealCard(normalAbilities)
 	    end
 
-	    for u = 1, ultimates do
+	    for u = 1, ultimatesPerHand do
 	       hand["u" .. i] = dealCard(ultimates)
 	    end
 
-	    for h = 1, heroes do
-	       hand["h" .. i] = dealCard(heroes)
+	    for h = 1, heroesPerHand do
+	       hand["h" .. i] = dealCard(allHeroes)
 	    end
 	 end
       end
-   end
-
-   print("all heroes")
-   for i, hero in ipairs(allHeroes) do
-      print("hero", i, hero)
-   end
-
-   print("all abilities")
-   for i, ability in ipairs(allAbilities) do
-      print("abilities", i, ability)
    end
 
    -- TODO: decide which things should be custom net tables
    -- then fire events to let the players know what hands they've been dealt.
 end
 
+-- TODO: we'd prefer not to have to distribute the npc_heroes.txt and npc_abilities.txt files ourselves - is there some better way?
 function loadHeroes()
    -- Returns a list of all the heroes in Dota 2.
-   -- TODO: can we use the dota_npc_units.txt file?
-   return LoadKeyValues("scripts/data/heroes.txt")["heroes"]
-end
+   local heroes = LoadKeyValues("scripts/data/npc_heroes.txt")
+   forbiddenHeroes = {"npc_dota_hero_base", "Version"}
+   local heroNames = {}
 
-function loadAbilities(heroes)
-   -- Given a list of heroes, returns the names of their abilities, divided into normal and ultimate abilities.
-
-   local normal = {}
-   local ultimates = {}
-
-   for i, heroname in ipairs(allHeroes) do
-      -- TODO: Create the hero temporarily in order to enumerate its abilities.
-      local hero = CreateUnitByName(heroname, vLocation, true, hNPCOwner, hUnitOwner, 1)
-
-      for i, hero in GetAbilityCount() do
-	 local ability = hero:GetAbilityByIndex(i)
-
-	 if ability:GetHeroLevelRequiredToUpgrade() > 1 then
-	    ultimates[#ultimates + 1] = ability:GetAbilityName()
-	 else
-	    normal[#normal + 1] = ability:GetAbilityName()
+   for name, _ in pairs(heroes) do
+      local ok = true
+      for i, badName in ipairs(forbiddenHeroes) do
+	 if name == badName then
+	    ok = false
+	    break
 	 end
       end
 
-      hero:RemoveSelf()
+      if ok then
+	 table.insert(heroNames, name)
+      end
+   end
+
+   return heroNames
+end
+
+function loadAbilities()
+   -- Returns a list of all the abilities in Dota 2, divided up by whether they are an ultimate or normal skill.
+   local normal = {}
+   local ultimates = {}
+
+   local abilities = LoadKeyValues("scripts/data/npc_abilities.txt")
+
+   for name, ability in pairs(abilities) do
+      if canPickAbility(name, ability) then
+	 if (ability["AbilityType"] == "DOTA_ABILITY_TYPE_ULTIMATE") then
+	    table.insert(ultimates, name)
+	 else
+	    table.insert(normal, name)
+	 end
+      end
    end
 
    return {normal = normal, ultimates = ultimates}
+end
+
+function canPickAbility(name, ability)
+   local forbiddenBehaviours = {"DOTA_ABILITY_BEHAVIOR_HIDDEN", "DOTA_ABILITY_BEHAVIOR_NOT_LEARNABLE", "DOTA_ABILITY_BEHAVIOR_ITEM"}
+   local forbiddenNames = {"Version", "ability_base"}
+
+   for _, forbidden in pairs(forbiddenNames) do
+      if (name == forbidden) then
+	 return false
+      end
+   end
+
+   local behaviour = ability["AbilityBehavior"]
+
+   if (behaviour == nil) then
+      return true
+   end
+
+   for _, forbidden in pairs(forbiddenBehaviours) do
+      if string.find(behaviour, forbidden) then
+	 return false
+      end
+   end
+   
+   return true
 end
 
 function selectHeroAndAbilities(playerID)
@@ -122,6 +158,8 @@ function selectHeroAndAbilities(playerID)
 	 ability
       )
    end
+
+   -- TODO: add sub-abilities
 
    hero.addAbility(hand["ultimate"])
 end
